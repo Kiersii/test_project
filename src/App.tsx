0be605ae5b38e94a0c766c3cf88909f1a3125267
kris <krzysiek002@gmail.com>
2022-05-16 16:32:31 +0200

    add alert button & repo link

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const handleShowAlert = () => {
+    alert("Hello React")
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +13,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <button onClick={handleShowAlert}>Hello</button>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +21,14 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+        </a>
+        <a
+            className="App-link"
+            href="https://github.com/Kiersii/test_project"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+          Github link
         </a>
       </header>
     </div>
